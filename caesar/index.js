dc71687983d4d5afe5c2f34742db1a7988ee5237
Roman Kovalenko <kovalenkoor@gmail.com>
2020-09-30 20:19:59 +0600

    initial commit

---
+++ caesar/index.js
@@ -0,0 +1,10 @@
+const { Command } = require("commander");
+
+const program = new Command();
+program.version("0.0.1");
+
+// program
+//   .option("-s, --shift", "a shift")
+//   .option("-i, --input", "an input file")
+//   .option("-o, --output", "an output file")
+//   .option("-a, --action", "an action encode/decode");
